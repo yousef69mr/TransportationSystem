866ad2c1c534089387be2bb6d4fd25fd5f3972f3
NermeenMohamed20190584 <84995327+NermeenMohamed20190584@users.noreply.github.com>
2022-01-02 23:09:05 +0200

    Update clientController.java

--- src/sw2phases/clientController.java
+++ src/sw2phases/clientController.java
@@ -1,20 +1,24 @@
 package com.example.demo.Users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(path="api/v1/client")
 public class clientController {
+    private final ClientService clientService;
+
+    @Autowired
+    public clientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
     @GetMapping
     public List<Client> getClient(){
-
-        BirthDay bd=new BirthDay();
-    return Arrays.asList(new Client("gggg", "01002337333", "aa@ff.com", "gggg", bd));
+    return ClientService.getClient();
 
     }
 
